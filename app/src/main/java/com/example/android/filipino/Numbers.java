3b75d208d67b37d7dce6a0c6f46ce8500e72805a
chrissetiana <chrissetiana@gmail.com>
2018-05-17 01:23:24 +0100

    Added activities

---
+++ app/src/main/java/com/example/android/filipino/Numbers.java
@@ -0,0 +1,13 @@
+package com.example.android.filipino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Numbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+    }
+}
---
+++ app/src/main/java/com/example/android/filipino/Phrases.java
@@ -0,0 +1,13 @@
+package com.example.android.filipino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Phrases extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+    }
+}
